cd68ac74419d671ba815cb979b619ca12f879ce8
muhsina.akter <muhsina.akter2@gmail.com>
2020-07-24 19:08:01 +0600

    Create database and add db files

---
+++ app/db.php
@@ -0,0 +1,23 @@
+<?php  
+	
+
+	/**
+	 * Database connection 
+	 */
+
+	$host = 'localhost'; 
+	$user = 'root';
+	$pass = '';
+	$db = 'ac';
+
+	$connection = new mysqli($host, $user, $pass, $db);
+
+
+
+
+
+
+
+
+
+?>
